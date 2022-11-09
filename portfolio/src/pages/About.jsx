d0859c52be68e8cf6d89b5678660aadb66ff1794
Maci Abesadze <maci.abesadze@gmail.com>
2022-11-09 15:52:12 +0300

    adding about

---
+++ portfolio/src/pages/About.jsx
@@ -0,0 +1,78 @@
+import React from "react";
+import styles from "./Pages.module.css";
+
+const About = () => {
+  return (
+    <div className={styles.about}>
+      <div>
+        <h1>Hello my name is Maci Abesadze</h1>
+        <br />
+        <br />
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iure dolorum
+          consequatur, tempore quidem odit veniam recusandae laboriosam quaerat
+          deserunt aut accusantium beatae officia? Architecto magni vero quasi
+          quis rerum nesciunt?
+        </p>
+        <br />
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iure dolorum
+          consequatur, tempore quidem odit veniam recusandae laboriosam quaerat
+          deserunt aut accusantium beatae officia? Architecto magni vero quasi
+          quis rerum nesciunt?
+        </p>
+        <br />
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iure dolorum
+          consequatur, tempore quidem odit veniam recusandae laboriosam quaerat
+          deserunt aut accusantium beatae officia? Architecto magni vero quasi
+          quis rerum nesciunt?
+        </p>
+        <br />
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iure dolorum
+          consequatur, tempore quidem odit veniam recusandae laboriosam quaerat
+          deserunt aut accusantium beatae officia? Architecto magni vero quasi
+          quis rerum nesciunt?
+        </p>
+        <br />
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iure dolorum
+          consequatur, tempore quidem odit veniam recusandae laboriosam quaerat
+          deserunt aut accusantium beatae officia? Architecto magni vero quasi
+          quis rerum nesciunt?
+        </p>
+        <br />
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iure dolorum
+          consequatur, tempore quidem odit veniam recusandae laboriosam quaerat
+          deserunt aut accusantium beatae officia? Architecto magni vero quasi
+          quis rerum nesciunt?
+        </p>
+        <br />
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iure dolorum
+          consequatur, tempore quidem odit veniam recusandae laboriosam quaerat
+          deserunt aut accusantium beatae officia? Architecto magni vero quasi
+          quis rerum nesciunt?
+        </p>
+        <br />
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iure dolorum
+          consequatur, tempore quidem odit veniam recusandae laboriosam quaerat
+          deserunt aut accusantium beatae officia? Architecto magni vero quasi
+          quis rerum nesciunt?
+        </p>
+        <br />
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iure dolorum
+          consequatur, tempore quidem odit veniam recusandae laboriosam quaerat
+          deserunt aut accusantium beatae officia? Architecto magni vero quasi
+          quis rerum nesciunt?
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default About;
