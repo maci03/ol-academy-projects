d43ba60276412165146c6fcb1ebd9320e5c8b71b
Maci Abesadze <maci.abesadze@gmail.com>
2022-11-09 16:17:16 +0300

    adding home page

---
+++ portfolio/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const homeStyles = {
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  minHeight: "80vh",
+};
+
+const Home = () => <h1 style={homeStyles}>Hello guys</h1>;
+
+export default Home;
