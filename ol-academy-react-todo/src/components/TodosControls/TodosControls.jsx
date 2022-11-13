c7a87c5ab35a8c8a6b8abc677997109ede2ef0e3
Maci Abesadze <maci.abesadze@gmail.com>
2022-11-13 23:19:38 +0300

    rewriting  ending

--- ol-academy-react-todo/src/components/TodosControls/TodosControls.jsx
+++ ol-academy-react-todo/src/components/TodosControls/TodosControls.jsx
@@ -1,23 +1,23 @@
 import React from "react";
-import styles from "./TodosContorls.module.css";
+import styles from "./TodosControls.module.css";
 import Button from "../Button/Button";
 
 class TodosContorls extends React.Component {
-  render() {
-    return (
-      <div className={styles.controls}>
-        <Button variant="danger" onClick={this.props.onDeleteAllTodos}>
-          Delete all todos
-        </Button>
-        <Button variant="success" onClick={this.props.onDeleteCompletedTodos}>
-          Delete completed todos
-        </Button>
-        <Button variant="primary" onClick={this.props.onDeleteCheckedTodos}>
-          Delete checked todos
-        </Button>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className={styles.controls}>
+                <Button variant="danger" onClick={this.props.onDeleteAllTodos}>
+                    Delete all todos
+                </Button>
+                <Button variant="success" onClick={this.props.onDeleteCompletedTodos}>
+                    Delete completed todos
+                </Button>
+                <Button variant="primary" onClick={this.props.onDeleteCheckedTodos}>
+                    Delete checked todos
+                </Button>
+            </div>
+        );
+    }
 }
 
 export default TodosContorls;
